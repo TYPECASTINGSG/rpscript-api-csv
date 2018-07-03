f09b147fd7165a0586d95c4c1dab3ae17ef5685e
James Chong <wei3hua2@gmail.com>
2018-07-03 15:42:11 +0800

    change verb naem

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 @RpsModule("csv")
 export default class RPSCsv {
 
-  @rpsAction({verbName:'parseCSV'})
-  async parseCSV (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
+  @rpsAction({verbName:'parseCsvToData'})
+  async parseCsvToData (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
     return parse(data,opts);
   }
 
-  @rpsAction({verbName:'parseCSV'})
-  async dataToCSV (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
+  @rpsAction({verbName:'parseDataToCsv'})
+  async parseDataToCsv (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
     return stringify(data,opts);
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -9,11 +9,11 @@ m.describe('CSV', () => {
   m.it('should display date correctly', async function () {
     let csv = new RPSCsv;
 
-    let output = await csv.parseCSV(new RpsContext,{},"h1,h2,h3\n1,2,3\n4,5,6\n");
+    let output = await csv.parseCsvToData(new RpsContext,{},"h1,h2,h3\n1,2,3\n4,5,6\n");
 
     c.expect(output).to.be.deep.equals( [[ 'h1', 'h2', 'h3' ], [ '1', '2', '3' ], [ '4', '5', '6' ] ]);
 
-    let outputStr = await csv.dataToCSV(new RpsContext,{},output);
+    let outputStr = await csv.parseDataToCsv(new RpsContext,{},output);
 
     c.expect(outputStr).to.be.equals("h1,h2,h3\n1,2,3\n4,5,6\n");
 
