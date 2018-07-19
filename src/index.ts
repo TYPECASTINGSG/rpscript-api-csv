67d940a9f0b6c656cf9fdbf7c780f333819b683b
James Chong <wei3hua2@gmail.com>
2018-07-19 18:08:07 +0800

    update csv module

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
-/**
- * @module CSV
- */
-
 import parse from 'csv-parse/lib/sync';
 import stringify from 'csv-stringify/lib/sync';
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
+/** Module for parsing CSV file
+ * @see {@link https://www.npmjs.com/package/figlet|CSV}
+ * @namespace CSV
+ * @example
+ * rps install csv
+*/
 @RpsModule("csv")
 export default class RPSCsv {
 
-  @rpsAction({verbName:'parseCsvToData'})
+/**
+ * @function convert-csv-to-data
+ * @memberof CSV
+ * 
+ * 
+ * 
+*/
+  @rpsAction({verbName:'convert-csv-string-to-data'})
   async parseCsvToData (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
     return parse(data,opts);
   }
 
-  @rpsAction({verbName:'parseDataToCsv'})
+    /**
+ * @function covert-data-to-csv-string
+ * @memberof CSV
+ * 
+ * 
+*/
+  @rpsAction({verbName:'convert-data-to-csv-string'})
   async parseDataToCsv (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
     return stringify(data,opts);
   }
