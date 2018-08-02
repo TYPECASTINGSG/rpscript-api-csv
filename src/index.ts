431abb59e516a6b0e9b4670cb17f919da4291053
James Chong <wei3hua2@gmail.com>
2018-08-02 20:43:38 +0800

    curry csv

--- src/index.ts
+++ src/index.ts
@@ -15,24 +15,43 @@ export default class RPSCsv {
  * @function convert-csv-to-data
  * @memberof CSV
  * @param {string} csvString csv content to be parsed
+ * @example
+ * convert-csv-to-data read-file "input.csv" --columns=true
+ * 
+ * 
+ * @summary convert-csv-to-data :: String -> String
+ * 
  * @description
  * For the list of parsing options:
- * 
  * @see {@link http://csv.adaltas.com/parse/}
 */
-  @rpsAction({verbName:'convert-csv-string-to-data'})
-  async parseCsvToData (ctx:RpsContext,opts:Object, content:string) : Promise<any>{
-    return parse(content,opts);
+  @rpsAction({verbName:'convert-csv-to-data'})
+  async parseCsvToData (ctx:RpsContext,opts:Object, csvString?:string) : Promise<any>{
+    if(csvString)
+      return parse(csvString,opts);
+    else
+      return function(csvString){return parse(csvString,opts);}
   }
 
-    /**
- * @function covert-data-to-csv-string
+/**
+ * @function covert-data-to-csv
  * @memberof CSV
+ * @param {string} data csv content to be parsed
+ * @example
+ * write-file "result.csv" convert-data-to-csv $processedResult
  * 
+ * @summary convert-data-to-csv :: String -> String
+ * 
+ * @description
+ * For the list of parsing options:
+ * @see {@link http://csv.adaltas.com/stringify/}
  * 
 */
-  @rpsAction({verbName:'convert-data-to-csv-string'})
-  async parseDataToCsv (ctx:RpsContext,opts:Object, data:any) : Promise<any>{
-    return stringify(data,opts);
+  @rpsAction({verbName:'convert-data-to-csv'})
+  async parseDataToCsv (ctx:RpsContext,opts:Object, data?:any) : Promise<any>{
+    if(data)
+      return stringify(data,opts);
+    else
+      return function(data){return stringify(data,opts);}
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -9,12 +9,16 @@ m.describe('CSV', () => {
   m.it('should display date correctly', async function () {
     let csv = new RPSCsv;
 
+    let fn = await csv.parseCsvToData(new RpsContext,{});
     let output = await csv.parseCsvToData(new RpsContext,{},"h1,h2,h3\n1,2,3\n4,5,6\n");
 
+    c.expect(fn("h1,h2,h3\n1,2,3\n4,5,6\n")).to.be.deep.equals(output);
     c.expect(output).to.be.deep.equals( [[ 'h1', 'h2', 'h3' ], [ '1', '2', '3' ], [ '4', '5', '6' ] ]);
 
+    let outputFn = await csv.parseDataToCsv(new RpsContext,{});
     let outputStr = await csv.parseDataToCsv(new RpsContext,{},output);
-
+    
+    c.expect(outputFn(output)).to.be.deep.equals(outputStr);
     c.expect(outputStr).to.be.equals("h1,h2,h3\n1,2,3\n4,5,6\n");
 
   }).timeout(0);
